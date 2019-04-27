584a475b180445582661361b2f679768516a19e9
Abishai Gray <linkabi9@gmail.com>
2019-04-27 16:39:31 -0400

    add Spruce UI dependencies;

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -75,6 +75,31 @@ function tryGitInit(appPath) {
   }
 }
 
+function installSpruceUiDependencies(useYarn, verbose) {
+
+  let command;
+  let args;
+
+  if (useYarn) {
+    command = 'yarnpkg';
+    args = ['add'];
+  } else {
+    command = 'npm';
+    args = ['install', '--save', verbose && '--verbose'].filter(e => e);
+  }
+  args.push('enzyme', 'mobx', 'mobx-react', 'prettier', 'ui-toolkit');
+
+  console.log(`Installing Spruce UI dependencies using ${command}...`);
+  console.log();
+
+  const proc = spawn.sync(command, args, { stdio: 'inherit' });
+  if (proc.status !== 0) {
+    console.error(`\`${command} ${args.join(' ')}\` failed`);
+    return;
+  }
+
+}
+
 module.exports = function(
   appPath,
   appName,
@@ -194,6 +219,8 @@ module.exports = function(
       return;
     }
   }
+
+  installSpruceUiDependencies(useYarn, verbose);
 
   if (useTypeScript) {
     verifyTypeScriptSetup();
