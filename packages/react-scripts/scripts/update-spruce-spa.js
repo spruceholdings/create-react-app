a571dfdb1264dd5989c5771bea5be53cc816218f
Abishai Gray <linkabi9@gmail.com>
2019-05-16 10:28:02 -0400

    include all the missing requires for update-spruce-spa

--- packages/react-scripts/scripts/update-spruce-spa.js
+++ packages/react-scripts/scripts/update-spruce-spa.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const fs = require('fs-extra');
+const path = require('path');
+const paths = require('../config/paths');
 const installSpruceSpaDependencies = require('./utils/installSpruceSpaDependencies');
-const useYarn = fs.existsSync(path.join(appPath, 'yarn.lock'));
+const useYarn = fs.existsSync(path.join(paths.appPath, 'yarn.lock'));
 const verbose = false; // pull verbose from command line params
 installSpruceSpaDependencies(useYarn, verbose);
