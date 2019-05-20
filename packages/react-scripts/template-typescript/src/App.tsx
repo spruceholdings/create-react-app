6756103c3f43cc964290727eadb79ad9e3739ba2
irinazotova <zotovaiv87@gmail.com>
2019-05-20 12:47:26 -0400

    Add IconsSource

--- packages/react-scripts/template-typescript/src/App.tsx
+++ packages/react-scripts/template-typescript/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Header from 'ui-toolkit/src/components/Header/Header';
+import IconsSource from 'ui-toolkit/src/components/Icon/IconsSource';
 import './_App.scss';
 
 const noop = () => void 0;
@@ -7,6 +8,7 @@ const noop = () => void 0;
 const App: React.FC = () => {
   return (
     <div className="App">
+      <IconsSource/>
       <Header onSignOutClick={noop}/>
     </div>
   );
